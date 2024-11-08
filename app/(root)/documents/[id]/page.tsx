5df4f7d9c47d589ed27f5483805d2b344affcc9b
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2024-11-08 20:37:29 +0200

    implementing basic liveblocks editor rooms

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,10 @@
-import Header from '@/components/Header'
-import { Editor } from '@/components/editor/Editor'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
-      <Header>
-        <div className='flex w-fit items-center justify-center gap-2'>
-<p className='document-title'>This is a fake document title</p>
-       </div>
-       <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor/>
+     
     </div>
   )
 }
